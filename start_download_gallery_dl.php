3e7128c5e655377a5a47d7942c1c45b4a5bd8e95
BloschDev <matthias-volkmann@gmx.net>
2024-07-15 18:36:35 +0200

    1

--- start_download_gallery_dl.php
+++ start_download_gallery_dl.php
@@ -3,7 +3,7 @@ session_start();
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $url = filter_var($_POST['url'], FILTER_SANITIZE_URL);
-    $destinationDir = "/var/www/html/download";
+    $destinationDir = "/data/dl";
 
     $command = "gallery-dl -d " . escapeshellarg($destinationDir) . " " . escapeshellarg($url) . " > /tmp/gallery-dl-output.txt 2>&1 & echo $!";
 
