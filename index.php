c42cd5d8c956be79211617a84daa6997109698b9
BloschDev <matthias-volkmann@gmx.net>
2024-07-15 16:21:18 +0200

    1

--- index.php
+++ index.php
@@ -119,15 +119,13 @@ body {font-family: Arial;}
   evt.currentTarget.className += " active";
 }
 
-########## YT-DLP ########
-
-        function startDownload() {
+        function startDownload2() {
             var url = document.getElementById("url").value;
             var iframe = document.getElementById("console");
             iframe.src = "download-yt.php?url=" + encodeURIComponent(url);
         }
 
-        function stopDownload() {
+        function stopDownload2() {
             fetch("stop-yt.php")
                 .then(response => response.text())
                 .then(data => alert(data));
@@ -159,11 +157,11 @@ body {font-family: Arial;}
 
 <div id="YT" class="tabcontent">
    <h1>YouTube Downloader</h1>
-    <form action="javascript:void(0);" method="post" onsubmit="startDownload()">
+    <form action="javascript:void(0);" method="post" onsubmit="startDownload2()">
         <label for="url">YouTube URL:</label>
         <input type="text" id="url" name="url" required>
         <button type="submit">Download</button>
-        <button type="button" onclick="stopDownload()">Stop</button>
+        <button type="button" onclick="stopDownload2()">Stop</button>
     </form>
     <iframe id="console" style="width: 100%; height: 400px;"></iframe>
 </div>
