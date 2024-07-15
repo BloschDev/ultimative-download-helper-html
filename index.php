dc65788ad1019639c95743d6161dfa8c7e995954
BloschDev <matthias-volkmann@gmx.net>
2024-07-15 16:19:38 +0200

    1

--- index.php
+++ index.php
@@ -118,6 +118,21 @@ body {font-family: Arial;}
   document.getElementById(cityName).style.display = "block";
   evt.currentTarget.className += " active";
 }
+
+########## YT-DLP ########
+
+        function startDownload() {
+            var url = document.getElementById("url").value;
+            var iframe = document.getElementById("console");
+            iframe.src = "download-yt.php?url=" + encodeURIComponent(url);
+        }
+
+        function stopDownload() {
+            fetch("stop-yt.php")
+                .then(response => response.text())
+                .then(data => alert(data));
+        }
+
     </script>
 </head>
 
@@ -143,8 +158,14 @@ body {font-family: Arial;}
 </div>
 
 <div id="YT" class="tabcontent">
-  <h3>Youtube-DL</h3>
-  <p>Youtube Downloader</p> 
+   <h1>YouTube Downloader</h1>
+    <form action="javascript:void(0);" method="post" onsubmit="startDownload()">
+        <label for="url">YouTube URL:</label>
+        <input type="text" id="url" name="url" required>
+        <button type="submit">Download</button>
+        <button type="button" onclick="stopDownload()">Stop</button>
+    </form>
+    <iframe id="console" style="width: 100%; height: 400px;"></iframe>
 </div>
 
 <div id="UPDATES" class="tabcontent">
