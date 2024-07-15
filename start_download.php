c705fe22aa00a79a75a31fa11fbed2d18ee2e537
BloschDev <matthias-volkmann@gmx.net>
2024-07-15 09:39:18 +0200

    Corrections

--- start_download.php
+++ start_download.php
@@ -6,7 +6,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $url = filter_var($_POST['url'], FILTER_SANITIZE_URL);
 
     // Zielverzeichnis
-    $destinationDir = "/var/www/html/download";
+    $destinationDir = "/data";
 
     // Befehl erstellen
     $command = "gallery-dl -d " . escapeshellarg($destinationDir) . " " . escapeshellarg($url) . " > /tmp/gallery-dl-output.txt 2>&1 & echo $!";
