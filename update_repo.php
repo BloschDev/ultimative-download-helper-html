a7584e554321a0814c4badad5dd652ac771ed477
BloschDev <matthias-volkmann@gmx.net>
2024-07-15 08:44:43 +0200

    Update

--- update_repo.php
+++ update_repo.php
@@ -4,7 +4,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $output = [];
     $return_var = 0;
 
-    exec('rm -rf /var/www/html/* && git clone https://github.com/BloschDev/ultimative-download-helper-html.git /tmp/repo && cp -r /tmp/repo/* /var/www/html/ && rm -rf /tmp/repo', $output, $return_var);
+    // Führen Sie den Befehl aus und erfassen Sie die Standardausgabe und die Fehlerausgabe
+    exec('rm -rf /var/www/html/* && git clone https://github.com/BloschDev/ultimative-download-helper-html.git /tmp/repo && cp -r /tmp/repo/* /var/www/html/ && rm -rf /tmp/repo 2>&1', $output, $return_var);
 
     // Ergebnis anzeigen
     if ($return_var === 0) {
