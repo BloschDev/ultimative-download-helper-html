cc3f9859ea2888805f151d98366e9ad3923f21cf
BloschDev <matthias-volkmann@gmx.net>
2024-07-15 16:37:57 +0200

    1

--- start_download_yt_dlp.php
+++ start_download_yt_dlp.php
@@ -3,9 +3,9 @@ session_start();
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $url = filter_var($_POST['url'], FILTER_SANITIZE_URL);
-    $outputDir = "/data/vdl";
+    $outputTemplate = "/data/vdl/%(channel)s - %(title)s.%(ext)s";
 
-    $command = "yt-dlp -o " . escapeshellarg($outputDir) . " " . escapeshellarg($url) . " > /tmp/yt-dlp-output.txt 2>&1 & echo $!";
+    $command = "yt-dlp -o " . escapeshellarg($outputTemplate) . " " . escapeshellarg($url) . " > /tmp/yt-dlp-output.txt 2>&1 & echo $!";
 
     $pid = shell_exec($command);
     $_SESSION['yt_dlp_pid'] = trim($pid);
