df8adbd90e44a0e2650550227442e1b7cd1ad30c
BloschDev <matthias-volkmann@gmx.net>
2024-07-16 06:27:16 +0200

    1

--- deploy.php
+++ deploy.php
@@ -5,7 +5,7 @@ $output = [];
 $return_var = 0;
 
 // Befehl zum Ausführen des Deploy-Skripts
-$deploy_script = '/var/www/html/deploy.sh';
+$deploy_script = '/var/www/deploy.sh';
 
 if (file_exists($deploy_script) && is_executable($deploy_script)) {
     exec("sudo $deploy_script > /tmp/deploy-output.txt 2>&1 & echo $!", $output, $return_var);
--- deploy.sh
+++ deploy.sh
@@ -5,12 +5,13 @@ git clone https://github.com/BloschDev/ultimative-download-helper-html.git /tmp/
 
 # Dateien kopieren
 cp -r /tmp/repo/* /var/www/html/
-
+mv /var/www/html/deploy.sh /var/www/
 # Temporäres Repository löschen
 rm -rf /tmp/repo
 
 # Berechtigungen ändern
 chmod -R 755 /var/www/html
+chmod 755 /var/www/deploy.sh
 
 # Besitzer ändern
 chown -R www-data:www-data /var/www/html
